fa678b748a6618d3a4c8f6c0680ffc4ff3ee7219
Jez McKinley <jezm@ibcos.co.uk>
2018-04-09 23:59:44 +0100

    JM: no longer need stop watch runner

--- eqalerter/stop_watch_runner.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import os
-import sys
-exec(compile(source=open('stop_watch.py').read(), filename='stop_watch.py', mode='exec'))
-stopwatch(sys.argv[1], sys.argv[2], sys.argv[3])
