22b5fe01a591ebf70c3f257045f530f333f1e614
Mark Nyqvist Hjarding (PeeWee) <peewee@hjarding.dk>
2021-06-26 08:58:44 -0400

    Use string formatter instead of addition.

--- eqalerter/delayed_message.py
+++ eqalerter/delayed_message.py
@@ -1,9 +1,14 @@
 from base_message import BaseMessage
 
+
 class DelayedMessage(BaseMessage):
 
     def __init__(self, timeout, message, title):
         self.message = message
         self.title = title
         self.timeout = timeout
-        self.command = 'urxvt -iconic -geometry 15x1 -bg red -fg white -title ' + title + ' -e python stop_watch.py ' + str(timeout) + ' "' + message + '" 1 "' + title + '"'
+        self.command = ('urxvt -iconic -geometry 15x1 -bg red -fg white'
+                        ' -title {title} -e python stop_watch.py {tmout}'
+                        ' "{msg}" 1 "{title}"').format(title=title,
+                                                      tmout=str(timeout),
+                                                      msg=message)
